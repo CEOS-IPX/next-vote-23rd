dc6f480c86cbe2cb058e8d26c3844159a858a356
girimNam <rlfla@naver.com>
2026-06-26 11:02:53 +0900

    chore: 스크롤바 숨김 처리

--- src/app/voting/layout.tsx
+++ src/app/voting/layout.tsx
@@ -6,9 +6,9 @@ export default function VotingLayout({
   children: React.ReactNode;
 }) {
   return (
-    <>
+    <div className="h-screen overflow-y-auto no-scrollbar">
       <NavBar className="absolute top-0 left-0 w-full z-10 md:flex md:justify-center md:mt-9" />
       {children}
-    </>
+    </div>
   );
 }
--- src/components/RankBadge.tsx
+++ src/components/RankBadge.tsx
@@ -5,12 +5,12 @@ type Props = {
 
 export default function RankBadge({ rank, color }: Props) {
   return (
-    <div className="relative flex items-center justify-center w-[86px] h-[86px] rounded-full bg-white border-2 border-[#191F28]">
+    <div className="relative flex items-center justify-center w-21.5 h-21.5 rounded-full bg-white border-2 border-[#191F28]">
       <div
-        className="absolute w-[33px] h-[33px] rounded-full blur-[20px]"
+        className="absolute w-8 h-8 rounded-full blur-xl"
         style={{ backgroundColor: color }}
       />
-      <span className="relative text-center text-[28px] font-bold leading-[135%] tracking-[-0.028px] text-black">
+      <span className="relative text-center text-3xl font-bold leading-[135%] tracking-[-0.028px] text-black">
         {rank}위
       </span>
     </div>
